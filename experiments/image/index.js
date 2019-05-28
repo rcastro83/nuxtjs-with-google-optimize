554e58b1c213fa1a407990dcc80d88d837ea4132
Rui Castro <ruicastro@iMacs-iMac.local>
2019-05-28 22:13:36 +0100

    add google analytics

--- experiments/image/index.js
+++ experiments/image/index.js
@@ -2,13 +2,14 @@ export default {
   name: 'image',
 
   // Google optimize experiment id
-  experimentID: '1dZJNHLTQmyj5uLZI5kf0A',
+  experimentID: 'wXIqINftRTGdpeNqY1LIKw',
 
   isEligible: ({ route }) => route.path === '/route1/',
 
   // Implemented variants and their weights
   variants: [
     { weight: 1 }, // <-- Control
-    { weight: 1 } // <-- Variation 1
+    { weight: 1 }, // <-- Variation 1
+    { weight: 2 } // <-- Variation 2
   ]
 }
